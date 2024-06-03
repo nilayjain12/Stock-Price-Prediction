f4db9a648752c499c9a4d8f30d0dc69445f6fcff
Nilay Jain <31623181+nilayjain12@users.noreply.github.com>
2024-06-03 16:15:01 -0700

    GRU and LSTM
    GRU and LSTM

---
+++ config.py
@@ -0,0 +1,2 @@
+# initializing the base path of the folder for ease
+BASE_DIR = r'C:\Users\nilay\OneDrive - Cal State Fullerton (1)\Desktop\NILAY-TO-JOB-DATA\SPRING 2024\CPSC 597 Project\Stock-Price-Prediction'
